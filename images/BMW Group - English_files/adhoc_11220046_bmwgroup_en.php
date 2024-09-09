7169328cb6e4c330a23017c782d99a0ea68b155b
Anugrah533 <anugrah5847@gmail.com>
2024-09-09 17:42:36 +0530

    first commit

---
+++ images/BMW Group - English_files/adhoc_11220046_bmwgroup_en.php
@@ -0,0 +1,5 @@
+
+			var psyma_desktop = document.createElement("script");
+			psyma_desktop.type = "text/javascript";
+			psyma_desktop.src = "//scripts.psyma.com/layer.php?xmlPath=adhoc/adhoc_11220046_bmwgroup_en";
+			document.body.appendChild(psyma_desktop);
